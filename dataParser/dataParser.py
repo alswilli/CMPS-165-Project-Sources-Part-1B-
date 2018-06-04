431a7ee956106a61a447ab192f2320a050b86174
vacio <kevinfserrano@gmail.com>
2018-06-04 14:00:59 -0700

    initializing the parser

--- dataParser/dataParser.py
+++ dataParser/dataParser.py
@@ -1,2 +1,8 @@
-msg = "Hello World"
-print(msg)
+import csv
+
+with open('name.csv', 'r') as csv_file: # 'r' for reading
+    csv_reader = csv.reader(csv_file)
+
+    for line in csv_reader:
+        print(line)
+        
