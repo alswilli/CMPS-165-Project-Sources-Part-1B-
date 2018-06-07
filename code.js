f0353281c295600e15f6d417723fa38fbe6622d3
DESKTOP-ABM275T\Alex Williamson <alexwilliamson123@gmail.com>
2018-06-07 13:02:04 -0700

    commented out equation

--- code.js
+++ code.js
@@ -586,6 +586,8 @@ function types2(d){
 			// Print the equation below the chart
 //			document.getElementsByClassName("equation")[0].innerHTML = "y = " + m + "x + " + b;
 //			document.getElementsByClassName("equation")[1].innerHTML = "x = ( y - " + b + " ) / " + m;
+        
+        console.log("working?")
 
 //      console.log("AHHHHHHH: " + ((minY - b) / m))
 //        console.log(Math.floor(((minY - b) / m)))
